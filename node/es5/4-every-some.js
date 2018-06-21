48e3c09dc1958bd00d5d753780d2eac3d2bfc459
Rondinelly <rondinelly-ribeiro@outlook.com>
2018-06-20 21:37:21 -0300

    Create 4-every-some.js

---
+++ node/es5/4-every-some.js
@@ -0,0 +1,35 @@
+module.exports = function checkUsersValid(goodUsers) {
+  return function allUsersValid(submittedUsers) {
+    return submittedUsers.every(function(submittedUser) {
+      return goodUsers.some(function(goodUser) {
+        return goodUser.id === submittedUser.id
+      })
+    })
+  }
+}
+
+//example
+
+var goodUsers = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 }
+]
+
+// `checkUsersValid` is the function you'll define
+var testAllValid = checkUsersValid(goodUsers)
+
+testAllValid([
+  { id: 2 },
+  { id: 1 }
+])
+// => true
+
+testAllValid([
+  { id: 2 },
+  { id: 4 },
+  { id: 1 }
+])
+// => false
+
+
