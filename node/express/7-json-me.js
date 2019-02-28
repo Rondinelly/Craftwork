c9ef643e8e5cc456a0bd8af0322b5b1946f64df9
Rondinelly <rod.melo32@gmail.com>
2019-02-28 17:30:10 -0300

    Create 7-json-me.js

---
+++ node/express/7-json-me.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+app.get("/books", function (req, res) {
+  const fileName = __dirname;
+  
+  fs.readFile(filename, function(e, data) {
+    
+    if (e)
+     return res.sendStatus(500);
+     
+     try {
+       books = JSON.parse(data);
+     } catch (e) {
+       res.sendStatus(500);
+     }
+     
+     res.json(books);
+  });
+});
+
+app.listen(3001);
