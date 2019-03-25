2ab8eeb8202a73d177a9a4c47ca02a58e9a48eaa
Rondinelly <rod.melo32@gmail.com>
2019-03-25 17:36:46 -0300

    Create 6-ping-bell.js

---
+++ node/johny-five/6-ping-bell.js
@@ -0,0 +1,18 @@
+const five = require("johnny-five");
+const dgram = require("dgram");
+const board = new five.Board();
+
+board.on("ready", function () {
+  let piezo = new five.Piezo(8);
+  let server = dgram.createSocket("udp4");
+
+  server.on("message", function () {
+    piezo.play({
+      song: 'C D F D A',
+      beats: 1 / 4,
+      tempo: 100
+    });
+  });
+
+  server.bind(1337);
+});
