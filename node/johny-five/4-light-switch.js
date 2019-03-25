ed9597c9b1b1a8049bfba00c3f51ea0f1d249134
Rondinelly <rod.melo32@gmail.com>
2019-03-25 18:09:27 -0300

    Update 4-light-switch.js

--- node/johny-five/4-light-switch.js
+++ node/johny-five/4-light-switch.js
@@ -1,11 +1,11 @@
 const five = require("johnny-five");
 const board = new five.Board();
 
-board.on("ready", function (){
+board.on("ready", () => {
   let led = new five.Led(9);
   let btn = new five.Button(5);
 
-  btn.on("press", function (){
-    led.toggle()
-  })
-})
+  btn.on("press", () => {
+    led.toggle();
+  });
+});
