e0892180d21b78cb635f7b878d23300c35d12eff
Rondinelly <rod.melo32@gmail.com>
2019-03-25 19:06:49 -0300

    Create 8-remote-temperature.js

---
+++ node/johny-five/8-remote-temperature.js
@@ -0,0 +1,25 @@
+const five = require("johnny-five");
+const dnode = require("dnode");
+
+const board = new five.Board();
+
+board.on("ready", () => {
+  let sensor = new five.Thermometer({
+    controller: "TMP36",
+    pin: "A0"
+  });
+  
+  let temp = null;
+
+  sensor.on("data", () => {
+    temp = this.celsius;
+  });
+
+  let server = dnode({
+    getTemperature: (cb) => {
+      cb(temp);
+    }
+  });
+
+  server.listen(1337);
+});
