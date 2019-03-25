26c1abcb96a7b0b0e251fddd47335a26029815e0
Rondinelly <rod.melo32@gmail.com>
2019-03-25 18:11:52 -0300

    Update 5-street-lamp.js

--- node/johny-five/5-street-lamp.js
+++ node/johny-five/5-street-lamp.js
@@ -1,15 +1,15 @@
 const five = require("johnny-five");
 const board = new five.Board();
 
-board.on("ready", function () {
+board.on("ready", () => {
   let led = new five.Led(9);
   let pr = new five.Sensor('A0');
 
-  pr.on("change", function () {
+  pr.on("change", () => {
     if (this.value > 600) {
-      led.on()
+      led.on();
     } else {
-      led.off()
+      led.off();
     }
-  })
-})
+  });
+});
