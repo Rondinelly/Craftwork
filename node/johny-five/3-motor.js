462e0c38350a7af970d9a22aa18013c9b184c574
Rondinelly <rod.melo32@gmail.com>
2019-03-25 18:08:15 -0300

    Update 3-motor.js

--- node/johny-five/3-motor.js
+++ node/johny-five/3-motor.js
@@ -1,20 +1,20 @@
 const five = require("johnny-five");
 const board = new five.Board();
 
-board.on("ready", function () {
-  let motor = new five.Motor(9)
+board.on("ready", () => {
+  let motor = new five.Motor(9);
 
-  motor.on("start", function () {
-    board.wait(2000, function () {
-      motor.stop()
-    })
-  })
+  motor.on("start", () => {
+    board.wait(2000, () => {
+      motor.stop();
+    });
+  });
 
-  motor.on("stop", function () {
-    board.wait(1000, function () {
-      motor.start(200)
-    })
-  })
+  motor.on("stop", () => {
+    board.wait(1000, () => {
+      motor.start(200);
+    });
+  });
 
-  motor.start(200)
-})
+  motor.start(200);
+});
